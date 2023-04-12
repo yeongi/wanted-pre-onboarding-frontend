2e2a67a8091cdd898dbdd6e33c5396bc4c8e2b36
yeongi <cbg5255@gmail.com>
2023-04-12 15:23:41 +0900

    feat : Todo 추가 기능 구현

--- src/component/todolist/Todo.tsx
+++ src/component/todolist/Todo.tsx
@@ -1,11 +1,17 @@
 import { TodoInfo } from "../../type/todo";
 
 const Todo = (props: TodoInfo) => {
+  const deleteTodo;
+
   return (
     <li>
       <label>
         <input type="checkbox" />
         <span>{props.todo}</span>
+
+        <button onClick={} data-testid="delete-button">
+          삭제
+        </button>
       </label>
     </li>
   );
